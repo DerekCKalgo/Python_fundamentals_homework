59fb31d6b38d344067b911798a21be07ff1bff5c
derekckalgo <derkoman@yahoo.com>
2020-12-08 14:00:21 -0800

    updated hello_world.py with hello world assignment of python fundamentals. Exercise on string manipulation.

---
+++ Hello_World2.py
@@ -0,0 +1,18 @@
+Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello word")
+hello word
+>>> print("hello", 9)
+hello 9
+>>> print("hello" + 9)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print("hello" + 9)
+TypeError: can only concatenate str (not "int") to str
+>>> print("helo" + "0")
+helo0
+>>> 
+= RESTART: /Users/derekkong/Desktop/python_stack/_python/python_fundamentals/hello_world.py
+Hello World
+>>> 
--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,11 @@
-x="Hello Python"
-print(x)
-y=42
-print(y)
+print("Hello World")
+name="Noelle"
+print("Hello", name,"!")
+print("Hello " + name + "!")
+name1=42
+print("Hello", name1, "!")
+print("Hello" + str(name1) + "!")
+fave_food1="sushi"
+fave_food2="pizza"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")
